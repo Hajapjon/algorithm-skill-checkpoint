4d3f72791d9531293bd4ddde91c9c0c7850cc114
nichalada <nichaladasuphachayanon@gmail.com>
2025-04-29 09:45:31 +0700

    feat: add answer to question 1-5

--- question-3.js
+++ question-3.js
@@ -1,20 +1,18 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
-
-
+Answer: Big O notation for Function findStudentById is O(n) because this function have to access all member in array.
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: Big O notation for Function findProductPrice is O log n because this function is not require to access all member in array
 
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
+Answer: Function findProductPrice is more efficient because since it's not require to access all member in array, So it can work faster than Function findStudentById.
 
 
 */
